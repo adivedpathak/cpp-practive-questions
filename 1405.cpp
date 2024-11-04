decf7c6c7e8366d5ed7a9a2f8c74095865a723cb
Aditya <aditya226vedpathk@gmail.com>
2024-11-04 21:42:32 +0530

    added another appoch of above

--- 1405.cpp
+++ 1405.cpp
@@ -43,3 +43,60 @@ int main() {
     }
  
 }
+
+
+
+***********************ADDed another approch **************************
+//vectors storing pairs used 
+// then for each start and end calculate
+// is there anyone who has intime before current and end time after current 
+// check for max between curr and orginal max
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int t;
+//     cin >> t;
+    
+//     while (t--) {
+//         int n;
+//         cin >> n;
+        
+//         vector<int> a(n), b(n);
+//         for (int i = 0; i < n; ++i) {
+//             cin >> a[i];
+//         }
+//         for (int i = 0; i < n; ++i) {
+//             cin >> b[i];
+//         }
+        
+//         vector<pair<int, int>> v(n);
+//         for (int i = 0; i < n; ++i) {
+//             v[i] = {a[i], b[i]};
+//         }
+        
+//         sort(v.begin(), v.end());
+        
+//         int answer = 1;
+//         for (int i = 0; i < n; ++i) {
+//             int start = v[i].first;
+//             int end = v[i].second;
+            
+//             for (int j = start; j < end; ++j) {
+//                 int count = 0;
+//                 for (int k = 0; k < n; ++k) {
+//                     if (v[k].first <= j && v[k].second > j) {
+//                         ++count;
+//                     }
+//                 }
+//                 answer = max(answer, count);
+//             }
+//         }
+        
+//         cout << answer << endl;
+//     }
+    
+//     return 0;
+// }
