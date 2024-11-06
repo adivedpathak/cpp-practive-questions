ee1b57b868c3b9c13bbd15bb2149a2d9655b4a47
aditya <aditya226vedpathak@gmail.com>
2024-11-06 11:46:58 +0530

    1425-Seconnd

--- 1415-2.cpp
+++ 1415-2.cpp
@@ -1,6 +1,31 @@
-this cde filefie code chef code 1415 2nd question
-szeifsdrh
-sjzvfsef'
-szdrjhdbzgr
-zdrjsgb
-zdkrjb
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+int t;
+cin>>t;
+while(t--){
+    int n;
+    int k;
+    cin>>n>>k;
+    std::vector<int>arr (n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    sort(arr.begin() ,arr.end());
+int mx =arr[n-1];
+int mn = arr[0];
+
+if(n==1 || mx + mn <= k){
+    cout<<"yes"<<endl;
+}else{
+    cout<<"no"<<endl;
+}
+   
+    
+}
+//just check mini + maxi <= k and size of an array
+
+}
