71e70d439f821346d4bca0420d4334d1343c82f7
aditya <aditya226vedpathak@gmail.com>
2024-11-07 19:55:21 +0530

    1423

---
+++ 1423.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+int t ;
+cin>>t;
+while(t--){
+    int l,r;
+     cin>>l>>r;
+if(r >= 2*l){
+    cout<<-1<<endl;
+}else{
+    cout<<r<<endl;
+}
+    
+}
+}
